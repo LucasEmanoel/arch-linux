db006f81e2c4ae5713950677f206961f96a60951
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-02-25 14:59:22 -0300

    Fix: adding new packages and fix some methods

--- base.sh
+++ base.sh
@@ -1,7 +1,45 @@
 #!/usr/bin/env bash
+source ./utils.sh
 
-function installBaseForDevOps(){
-    echo "---- Install Packages for DevOps ----"
-    
+function basePackages(){
+  echo "---- Installing Base Packages ----"
+  installPackage "$(cat packages/base.list)"
 }
 
+
+function personalPackages(){
+  echo "---- Installing My Personal Packages ----"
+  installPackage "$(cat packages/snap-classic.list)" "sudo snap install --classic"
+  installPackage "$(cat packages/snap.list)" "sudo snap install"
+  yay -S --needed --noconfirm - < packages/yay.list
+}
+
+function devOpsPackages(){
+  #using pacman
+  installPackage "$(cat packages/devops.list)"
+}
+
+function configureDockerAndKubernetes(){
+  #run commands without sudo
+  usermod -aG libvirt $(whoami)
+  usermod -aG docker $(whoami)
+  
+  #set driver minikube
+  minikube config set driver kvm2
+  
+  #start services
+  sudo systemctl enable --now libvirtd.service
+  sudo systemctl enable --now docker.service
+}
+
+function main() {
+  basePackages
+  packagesManagers
+  configurePackagesManagers
+  personalPackages
+  dockerAndKubernetes
+  fontsInstall
+  configureZshAndTheme
+}
+
+main
--- utils.sh
+++ utils.sh
@@ -1,61 +1,69 @@
 #!/usr/bin/env bash
 
-function installManagerPackages(){
-    git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-    git clone https://aur.archlinux.org/snapd.git && cd snapd && makepkg -si
+function installPackage() {
+  local _apps=($1)
+  if [[ $# -eq 1 ]]; then
+    local _installBlock="sudo pacman -S --needed --noconfirm - "
+  else
+    local _installBlock="$2"
+  fi
+
+  for key in ${!_apps[@]}; do
+    eval $_installBlock ${_apps[$key]}
+  done
+}
+
+function packagesManagers(){
+  echo "---- Installing Packages Managers ----"
+
+  git clone https://aur.archlinux.org/yay.git ~/PackagesManagers/yay 
+  pushd ~/PackagesManagers/yay
+  makepkg -si
+  popd
+  
+  git clone https://aur.archlinux.org/snapd.git ~/PackagesManagers/snapd 
+  pushd ~/PackagesManagers/snapd 
+  makepkg -si
+  popd
 }
 
 function configurePackagesManagers(){
-
-    sudo systemctl enable --now snapd.socket
-    sudo ln -s /var/lib/snapd/snap /snap
-
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
 }
 
-function installPackages(){
-    local _apps=($1)
-    #verify if number args = 1
-    if [[ $# -eq 1]]; then
-        #--needed - dont reinstall packages up to date
-        #--noconfirm - set yes for all
-        local _installBlock="sudo pacman -S --needed --noconfirm"
-    else
-        local _installBlock="$2"
-    fi
-
-    for i in ${!_apps[@]}; do
-        eval $_installBlock ${_apps[$i]}
-    done
-
+function fontsInstall(){
+  echo "---- Installing Fonts ----"
+  mkdir -p ~/.local/share/fonts
+  mkdir -p ~/.local/fonts/MesloLGS/
+  
+  pushd ~/.local/share/fonts
+  curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+  popd
+  
+  pushd ~/.local/fonts/MesloLGS/
+  wget -c "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+  wget -c "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+  wget -c "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+  wget -c "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+  popd
 }
 
-function basePackages(){
-    #set multlib repository
-    installPackage < packages/base.list
+function configureZshAndTheme(){
+  #set zsh shell for my user
+  chsh --shell $(which zsh)
 
+  echo "------- OhMyZsh installing ------"
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  
+  sudo sed -i 's/^ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+  echo "------- OhMyZsh installing powerlevel10k ------"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
+  
+  local _zshPlugins="(git zsh-autosuggestions zsh-syntax-highlighting asdf nvm node docker)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  sudo sed -i "s/^plugins=.*/plugins=$_zshPlugins/" ~/.zshrc
+  echo "!!!!!logout/login system to works!!!!"
 }
-
-
-function personalPackages(){
-    installPackage ""
-}
-
-function dockerAndKubernetes(){
-    installPackage ""
-}
-
-function configureDockerAndKubernetes(){
-    #run commands without sudo
-    usermod -aG libvirt $(whoami)
-    usermod -aG docker $(whoami)
-
-    #set driver minikube
-    minikube config set driver kvm2 
-
-    #start services
-    sudo systemctl enable --now libvirtd.service
-    sudo systemctl enable --now docker.service
-
-
-}
-
